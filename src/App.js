6b676e2aeabca85e2d1f0589f64e8149c8f6be72
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-11-01 16:02:39 +0900

    Defining global styles

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import 'https://fonts.googleapis.com/css2?family=Outfit:wght@300;500&display=swap';
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
