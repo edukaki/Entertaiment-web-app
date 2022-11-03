8a5a2eca349aff6094a58c5038cc653304879331
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-11-04 07:02:59 +0900

    feat(Layout): add active class to the Navlink

---
+++ src/Pages/Layout.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { BrowserRouter, NavLink, Outlet } from 'react-router-dom'
+import { ReactComponent as AllIcon } from '../img/icon-nav-home.svg'
+import { ReactComponent as MoviesIcon } from '../img/icon-nav-movies.svg'
+import { ReactComponent as SeriesIcon } from '../img/icon-nav-tv-series.svg'
+import { ReactComponent as FavoritesIcon } from '../img/icon-nav-bookmark.svg'
+import avatar from '../img/image-avatar.png'
+
+import './Layout.css'
+
+const Layout = () => {
+
+    return (
+        <BrowserRouter>
+            <nav className='c-navbar'>
+                <div className='c-navbar__logo'></div>
+                <ul className='c-navbar__pages'>
+                    <li><NavLink
+                        to="/"
+                        className={({ isActive }) =>
+                            isActive ? 'c-navbar__icon active' : 'c-navbar__icon'}>
+                        <AllIcon /></NavLink></li>
+                    <li><NavLink to="/movies"
+                        className={({ isActive }) =>
+                            isActive ? 'c-navbar__icon active' : 'c-navbar__icon'}>
+                        <MoviesIcon /></NavLink></li>
+                    <li><NavLink to="/tv-series"
+                        className={({ isActive }) =>
+                            isActive ? 'c-navbar__icon active' : 'c-navbar__icon'}>
+                        <SeriesIcon /></NavLink></li>
+                    <li><NavLink to="/bookmarks"
+                        className={({ isActive }) =>
+                            isActive ? 'c-navbar__icon active' : 'c-navbar__icon'}>
+                        <FavoritesIcon /></NavLink></li>
+                </ul>
+                <div><img className='c-navbar__avatar' src={avatar} alt='avatar' /></div>
+            </nav>
+            <Outlet />
+        </BrowserRouter>
+
+    )
+}
+
+export default Layout
+
+
+
