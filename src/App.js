143e30a116317c4594f779cefe45fbccc96aafef
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-11-04 11:28:00 +0900

    feat(App): add Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
-import Thumbnail from "./components/Thumbnail"
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import Layout from "./Pages/Layout";
 
 function App() {
   return (
-    <div className="App">
-        <Thumbnail />
-    </div>
+    <Router>
+      <Layout />
+      <Routes>
+        <Route path="/" element={<Layout />} />
+        <Route index element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
