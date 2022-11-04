dbb563404d66fdcfed85c7cd320733589805fa9a
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-11-04 11:29:28 +0900

    fix(Layout): remove BrowserRouter

--- src/Pages/Layout.jsx
+++ src/Pages/Layout.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, NavLink, Outlet } from 'react-router-dom'
+import { NavLink, Outlet } from 'react-router-dom'
 import { ReactComponent as AllIcon } from '../img/icon-nav-home.svg'
 import { ReactComponent as MoviesIcon } from '../img/icon-nav-movies.svg'
 import { ReactComponent as SeriesIcon } from '../img/icon-nav-tv-series.svg'
@@ -11,7 +11,7 @@ import './Layout.css'
 const Layout = () => {
 
     return (
-        <BrowserRouter>
+        <>
             <nav className='c-navbar'>
                 <div className='c-navbar__logo'></div>
                 <ul className='c-navbar__pages'>
@@ -36,7 +36,7 @@ const Layout = () => {
                 <div><img className='c-navbar__avatar' src={avatar} alt='avatar' /></div>
             </nav>
             <Outlet />
-        </BrowserRouter>
+        </>
 
     )
 }
