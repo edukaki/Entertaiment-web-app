00c459f55132bd1904310bfefc85d51a9ad57c9e
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-11-05 18:55:14 +0900

    feat(thumbnail): add img by props

--- src/components/Thumbnail/index.js
+++ src/components/Thumbnail/index.js
@@ -3,17 +3,16 @@ import BtnPlay from '../BtnPlay';
 import Bookmark from '../Bookmark';
 import "./Thumbnail.css"
 
-import example from "../../img/thumbnails/112/regular/small.jpg"
 function onClickBook(){
     console.log("clicked")
 }
 
-const Thumbnail = () => {
+const Thumbnail = (props) => {
     const [isShown, setIsShown] = useState((false));
     return (
         <>
             <div className='c-thumbnail__container' onMouseEnter={() => setIsShown(true)} onMouseLeave={() => setIsShown(false)}>
-                <img src={example} alt="Thumbnail" />
+                <img src={props.img} alt="thumb"/>
                 <Bookmark onClick={onClickBook}/>
                 {isShown && (
                     <BtnPlay />
