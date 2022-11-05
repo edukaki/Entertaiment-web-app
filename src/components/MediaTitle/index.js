6b5e18ea09ab0f9e69b6c88556f12b4f5684432d
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-11-05 18:58:07 +0900

    feat(media title): add title class

--- src/components/MediaTitle/index.js
+++ src/components/MediaTitle/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import './MediaTitle.css'
 
 const MediaTitle = (props) => {
+    const icon = props.data.category === "TV Series" ? "tv" : props.data.category;
     return (
-        <div>
-            <p>{props.data.year}&nbsp;&nbsp;&#183;&nbsp;&nbsp;<img src={`/img/icon-category-${props.data.category}.svg`} alt={props.data.category} />&nbsp;&nbsp;{props.data.category}&nbsp;&nbsp;&#183;&nbsp;&nbsp;{props.data.rating}</p>
+        <div className='c-media-title'>
+            <p>{props.data.year}&nbsp;&nbsp;&#183;&nbsp;&nbsp;<img src={`/img/icon-category-${icon}.svg`} alt={props.data.category} />&nbsp;&nbsp;{props.data.category}&nbsp;&nbsp;&#183;&nbsp;&nbsp;{props.data.rating}</p>
             <h2>{props.data.title}</h2>
         </div>
     )
