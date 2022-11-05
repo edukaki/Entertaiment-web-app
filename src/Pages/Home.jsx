0d8914e927d588c1a22025d59987432a9510eeaf
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-11-05 19:30:25 +0900

    refactor(Home): add gallery

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
-import SMovieCard from '../components/SMovieCard'
+import Gallery from '../components/Gallery'
 import Title from '../components/Title'
 
 import data from '../data/data.json'
 
 const Home = () => {
-    const categoryImg = data[0].category === "Movie" ? '/src/img/icon-category-movie.svg' : '';
 
     return (
         <>
             <Title text="Recommended for you"/>
-            <SMovieCard element={data[0]} categoryImg={categoryImg} />
+            <Gallery data={data} />
         </>
     )
 }
