010f73253e6d2a133a76ebd71dd9aac00b9869d7
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-11-06 22:17:41 +0900

    feat(app): add route to the new pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Bookmarked from "./Pages/Bookmarks";
 import Home from "./Pages/Home";
 import Layout from "./Pages/Layout";
+import Movies from "./Pages/Movies";
+import TVSeries from "./Pages/TVSeries";
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />} />
         <Route index element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/tv-series" element={<TVSeries />} />
+        <Route path="/bookmarks" element={<Bookmarked />} />
       </Routes>
     </Router>
   );
