a7dd10e2995c72317de7bde76de550c7e263a075
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-11-07 11:01:22 +0900

    fix(bookmark): fill bookmark after click

--- src/components/Bookmark/index.js
+++ src/components/Bookmark/index.js
@@ -1,26 +1,29 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import './Bookmark.css'
 
 const Bookmark = (props) => {
-  const [fill, setFill] = useState("none");
+  const [fill, setFill] = useState(props.bookmarked === true ? "#FFF" : "none");
   const [stroke, setStroke] = useState("#FFF");
-  const [active, setActive] = useState(false)
+
+  const navigate = useNavigate()
+  const refreshPage = () => { navigate(0) }
 
   return (
     <div className='c-bookmark__container'>
       <div className='c-bookmark'>
-        <svg className='c-bookmark-icon' width="12" height=".8rem" xmlns="http://www.w3.org/2000/svg"
+        <svg className='c-bookmark-icon' width="12" height=".8rem" xmlns="http://www.w3.org/2000/svg" fill={fill}
           onMouseEnter={() => {
             setFill("#FFF")
             setStroke("#000")
           }}
           onMouseLeave={() => {
-            setFill(() => !active ? "none" : "#FFF")
+            setFill(() => !props.bookmarked ? "none" : "#FFF")
             setStroke("#FFF")
           }}
           onClick={(event) => {
             props.onClick(event);
-            setActive(!active);
+            refreshPage()
             setFill("#FFF")
           }}
         ><path d="m10.518.75.399 12.214-5.084-4.24-4.535 4.426L.75 1.036l9.768-.285Z" stroke={stroke} strokeWidth="1.5" fill={fill} /></svg>
